fead418dd96b58a351b44a499ea65e42c18f47af
Micah Winkelspecht <winkelspecht@gmail.com>
2011-08-20 22:52:57 -0700

    new page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,11 @@ class PagesController < ApplicationController
       { :date => Date.today + 21.days, :name => 'Loan', :description => 'Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.' },
       { :date => Date.today + 29.days, :name => 'Close Escrow', :description => 'Donec semper quam scelerisque tortor dictum gravida. In hac habitasse platea dictumst. Nam pulvinar, odio sed rhoncus suscipit, sem diam ultrices mauris, eu consequat purus metus eu velit. Proin metus odio, aliquam eget molestie nec, gravida ut sapien. Phasellus quis est sed turpis sollicitudin venenatis sed eu odio. Praesent eget neque eu eros interdum malesuada non vel leo. Sed fringilla porta ligula.' }
     ]
+    
+    respond_to do |wants|
+      wants.html
+      wants.js
+    end
   end
   
   def admin
@@ -17,5 +22,22 @@ class PagesController < ApplicationController
       { :address_1 => '130 Catamaran St', :address_2 => '', :city => 'Los Angeles', :state => 'CA', :zip => '90094' },
       { :address_1 => '4141 Glencoe Ave', :address_2 => '', :city => 'Los Angeles', :state => 'CA', :zip => '90094' },
     ]
+    respond_to do |wants|
+      wants.html
+      wants.js
+    end
+  end
+  
+  def login
+    @person_id = params[:id]
+  end
+  
+  def create_session
+    @person_id = params[:id]
+    
+    respond_to do |wants|
+      wants.html
+      wants.js
+    end
   end
 end
